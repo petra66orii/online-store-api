1e7a7a8dfc06aa5784667e8b7efec9d805796686
Petra <petra66orii@gmail.com>
2025-07-25 19:02:12 +0100

    Add serializers for authentication

---
+++ users/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers, generics
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
