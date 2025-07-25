a03a25f5d12f418e104469b81f7c90f8a729c76b
Petra <petra66orii@gmail.com>
2025-07-25 19:01:49 +0100

    Add auth views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    return Response({'message': f'Hello, {request.user.username}!'})
+
+
+@api_view(['POST'])
+def register_user(request):
+    data = request.data
+    if User.objects.filter(username=data['username']).exists():
+        return Response({'detail': 'User already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(
+        username=data['username'],
+        email=data['email'],
+        password=data['password']
+    )
+
+    return Response({'detail': 'User created'})
