d45417388f0d0eda0b913badcdc9dd28e6640af4
Petra <petra66orii@gmail.com>
2025-07-26 15:23:06 +0100

    Add email verification URLs

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from users.serializers import RegisterView
-from users.views import protected_view
+from users.views import protected_view, verify_email
 
 
 urlpatterns = [
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/protected/', protected_view, name='protected'),
+    path('api/verify-email/', verify_email, name='email-verify'),
+
 ]
