57726ba80668cd8ece1afbadf8585bebdde025a8
Petra <petra66orii@gmail.com>
2025-07-28 18:48:34 +0100

    Add rate-limit settings, #4

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.UserRateThrottle",
+        "rest_framework.throttling.AnonRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "user": "10/minute",
+        "anon": "5/hour",
+    }
 }
 
 SIMPLE_JWT = {
