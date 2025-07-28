b1b85f3f854924df233e512b08cd0120c8ddf08b
Petra <petra66orii@gmail.com>
2025-07-28 18:49:46 +0100

    Add password reset serializer, #3

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers, generics
+from django.contrib.auth import get_user_model
+from .models import PasswordResetToken
+
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +17,34 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        if not User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("No user with this email.")
+        return value
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    token = serializers.UUIDField()
+    new_password = serializers.CharField(write_only=True, min_length=8)
+
+    def validate_token(self, value):
+        try:
+            self.token_obj = PasswordResetToken.objects.get(token=value)
+        except PasswordResetToken.DoesNotExist:
+            raise serializers.ValidationError("Invalid token.")
+        if self.token_obj.is_expired():
+            self.token_obj.delete()
+            raise serializers.ValidationError("Token expired.")
+        return value
+
+    def save(self):
+        user = self.token_obj.user
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        self.token_obj.delete()
+        return user
